7146e8e38d39921b76fa105f30e8f0340a59eeeb
Sai Karthik <venkatasaikarthikeya@gmail.com>
2023-10-14 19:42:41 -0700

    basic data echoing

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,16 @@
+import json
 from django.http import JsonResponse
 
 
-def api_home(requests, *args, **kwargs):
-    response = {'query': 'Hello World'}
-    return JsonResponse(response)
+# request -> It is an instance of HttpRequest -> Django # Nothing to do with python requests
+# request.body gives byte string JSON data
+
+def api_home(request, *args, **kwargs):
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data.keys())
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,5 +2,14 @@ import requests
 
 endpoint = 'http://127.0.0.1:8000/api/'
 
-response = requests.get(endpoint)
+request_json = {'Client Message': 'Hello Server!!'}
+request_params = {'abc': 123}
+response = requests.get(endpoint, params=request_params, json=request_json)
+
+text_response = response.text           # The response is directly converted into raw text
 print(response.text)
+
+dict_response = response.json()         # The response is converted into Python dictionary
+print(dict_response)
+
+print(f"Status Code of the request: ${response.status_code}")           # Gives the status code of the request
